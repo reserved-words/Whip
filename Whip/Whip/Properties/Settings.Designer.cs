6830821bc7105415b63e4451be501504a0381fb1
Rhian <rhian.gwawr@gmail.com>
2017-04-07 19:22:41 +0100

    Improved appearance of library tab and grouped releases by type

--- Whip/Whip/Properties/Settings.Designer.cs
+++ Whip/Whip/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Whip.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\rhian\\Music\\Test")]
         public string MusicDirectory {
             get {
                 return ((string)(this["MusicDirectory"]));
