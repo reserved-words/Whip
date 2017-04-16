bb9c042f03b42b83ce842700f8b27ec9106d6379
Rhian <rhian.gwawr@gmail.com>
2017-04-16 22:11:59 +0100

    Reduced delay before playing tracks to 500ms

--- Whip/Whip.Common/ApplicationSettings.cs
+++ Whip/Whip.Common/ApplicationSettings.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Whip.Common
 {
     public static class ApplicationSettings
     {
         public static string[] FileExtensions => new[] { ".mp3", ".m4a" };
-        public static int TrackChangeDelay => 800;
+        public static int TrackChangeDelay => 500;
     }
 }
