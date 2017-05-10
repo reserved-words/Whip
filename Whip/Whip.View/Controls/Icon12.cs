b930403c4be93738c0d893ffd75c3d3663aa25bd
Rhian <rhian.gwawr@gmail.com>
2017-05-10 17:12:54 +0100

    Playlists - added display templates for criteria and ordered playlists

---
+++ Whip/Whip.View/Controls/Icon12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whip.Controls
+{
+    public class Icon12 : FontAwesome.WPF.ImageAwesome
+    {
+        public Icon12()
+            : base()
+        {
+            VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            Width = 12;
+            Height = 12;
+            SnapsToDevicePixels = true;
+        }
+    }
+}
