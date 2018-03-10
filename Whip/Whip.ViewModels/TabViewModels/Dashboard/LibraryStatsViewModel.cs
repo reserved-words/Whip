75abbd6822851b786d6526502459f1d82158031f
Rhian <rhian.gwawr@gmail.com>
2018-03-10 11:47:22 +0000

    Added library statistic to dashboard (currently mock data)
    Related Work Items: #185

---
+++ Whip/Whip.ViewModels/TabViewModels/Dashboard/LibraryStatsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using Whip.Common;
+
+namespace Whip.ViewModels.TabViewModels.Dashboard
+{
+    public class LibraryStatsViewModel : ViewModelBase
+    {
+        public LibraryStatsViewModel()
+        {
+            NumberOfTrackArtists = 150;
+            NumberOfAlbumArtists = 100;
+            NumberOfAlbums = 1000;
+            NumberOfTracks = 10000;
+            AlbumsByReleaseType = new List < Statistic >
+            {
+                new Statistic(ReleaseType.Album.GetReleaseTypeGroupingDisplayName(), 381),
+                new Statistic(ReleaseType.Single.GetReleaseTypeGroupingDisplayName(), 15),
+                new Statistic(ReleaseType.BestOf.GetReleaseTypeGroupingDisplayName(), 21)
+            };
+            ArtistsByGrouping = new List< Statistic >
+            {
+                new Statistic("Metal", 38),
+                new Statistic("Pop", 35),
+                new Statistic("Alternative", 21)
+            };
+            TotalTime = new TimeSpan(5,5,2,16);
+        }
+
+        public int NumberOfTrackArtists { get; set; }
+        public int NumberOfAlbumArtists { get; set; }
+        public int NumberOfAlbums { get; set; }
+        public int NumberOfTracks { get; set; }
+        public List<Statistic> AlbumsByReleaseType { get; set; }
+        public List<Statistic> ArtistsByGrouping { get; set; }
+        public TimeSpan TotalTime { get; set; }
+
+        public class Statistic
+        {
+            public Statistic(string caption, int count)
+            {
+                Caption = caption;
+                Count = count;
+            }
+
+            public string Caption { get; }
+            public int Count { get; }
+        }
+    }
+}
--- Whip/Whip.ViewModels/TabViewModels/DashboardViewModel.cs
+++ Whip/Whip.ViewModels/TabViewModels/DashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Whip.Common;
+﻿using System.Security.Cryptography.X509Certificates;
+using Whip.Common;
+using Whip.ViewModels.TabViewModels.Dashboard;
 using Whip.ViewModels.Utilities;
 
 namespace Whip.ViewModels.TabViewModels
@@ -8,7 +10,9 @@ namespace Whip.ViewModels.TabViewModels
         public DashboardViewModel()
             :base(TabType.Dashboard, IconType.Home, "Dashboard")
         {
-
+            LibraryStatsViewModel = new LibraryStatsViewModel();
         }
+
+        public LibraryStatsViewModel LibraryStatsViewModel { get; }
     }
 }
