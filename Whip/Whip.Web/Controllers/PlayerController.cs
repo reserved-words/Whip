17e881eddb327ba4709c13ff92a0e44b30d3f415
Rhian <rhian.gwawr@gmail.com>
2018-06-17 12:15:54 +0100

    Fixed scrobbling issue
    Related Work Items: #363

--- Whip/Whip.Web/Controllers/PlayerController.cs
+++ Whip/Whip.Web/Controllers/PlayerController.cs
@@ -3,29 +3,34 @@ using System.Web.Mvc;
 using Whip.Common.Interfaces;
 using Whip.Services.Interfaces;
 using Whip.Services.Interfaces.Singletons;
+using Whip.Web.Interfaces;
 
 namespace Whip.Web.Controllers
 {
     public class PlayerController : BaseController
     {
         private readonly IPlayer _player;
+        private readonly IUpdatePlayProgress _playProgress;
 
-        public PlayerController(IPlayer player, ICloudService cloudService, IPlaylist playlist, IErrorLoggingService logger)
+        public PlayerController(IPlayer player, ICloudService cloudService, IPlaylist playlist, IErrorLoggingService logger, IUpdatePlayProgress playProgress)
             :base(cloudService, playlist, logger)
         {
             _player = player;
+            _playProgress = playProgress;
         }
 
         [HttpPost]
-        public HttpStatusCodeResult Play()
+        public HttpStatusCodeResult Play(int secondsPlayed)
         {
+            _playProgress.StartNewTrack(secondsPlayed, (int)Playlist.CurrentTrack.Duration.TotalSeconds);
             _player.Play(Playlist.CurrentTrack);
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
         [HttpPost]
-        public HttpStatusCodeResult Pause()
+        public HttpStatusCodeResult Pause(int secondsPlayed)
         {
+            _playProgress.UpdateSecondsPlayed(secondsPlayed);
             _player.Pause();
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
@@ -40,13 +45,15 @@ namespace Whip.Web.Controllers
         [HttpPost]
         public HttpStatusCodeResult SkipToPercentage(double percentage)
         {
+            _playProgress.UpdatePercentagePlayed(percentage);
             _player.SkipToPercentage(percentage);
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
         [HttpPost]
-        public HttpStatusCodeResult Stop()
+        public HttpStatusCodeResult Stop(int secondsPlayed)
         {
+            _playProgress.Stop(secondsPlayed);
             _player.Stop();
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
